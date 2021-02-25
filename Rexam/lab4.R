02f0068ee4dee51bfc8f1fc82d6b5b7af9511ec1
kdragonkorea <morenmore6340@gmail.com>
2021-02-25 23:22:47 +0900

    lab4 복습

--- Rexam/lab4.R
+++ Rexam/lab4.R
@@ -4,7 +4,8 @@ L1 = list(
   sal = 3000
 )
 
-L1$sal * 2 -> result1
+L1$sal * 2 -> result1 # L1[["sal"]] 가능
+result1
 
 # ex) L1['sal'] * 2 # L1['sal']: 리스트 형태 (이항연산자 불가)
 # ex) L1[['sal']] * 2 # L1[['sal']]: 벡터 (이항연산자 불가)
@@ -29,11 +30,11 @@ L4 = 	list(
   beta=sqrt(1:5), 
   gamma=log(1:5)
 )
-L4$alpha + 10
+L4$alpha + 10 # 다음과 같이 해도 가능: L4[[1]] + 10, L4[['alpha']] + 10
 
 # 문제5
 data1 = LETTERS
-data2 = data.frame(emp)[1:3,]
+data2 = data.frame(emp)[1:3,] # emp[1:3,] 으로 해도 가능
 data3 = L4
 
 L5 = list(
@@ -73,10 +74,24 @@ if (choice == 1){
 }else if (choice == 3){
   cat("결과값",":",300*50)
 }else if (choice == 4){
-  cat("결과값",":",300/50)
+  cat("결과값",":",300/50) 
 }else {
   cat("결과값",":",300%%50)
 }
+
+# 문제8-정답2
+if(choice==1){
+  msg = (300 + 50)
+}else if (choice==2){
+  msg = (300 - 50)
+}else if (choice==3){
+  msg = (300 * 50)
+}else if (choice==4){
+  msg = (300 / 50)
+}else {
+  msg = (300 %% 50)
+}
+cat("결과값 :" , msg)
 
 # 문제9 / hint: rep 함수이용
 (count = sample(3:10,1))
@@ -88,6 +103,18 @@ if(deco == 1){
   rep("$",count)
 }else {
   rep("#",count)
+}
+
+# 문제9 - 정답2
+if (deco==1){
+  for(i in 1:count)
+    cat("*")
+}else if (deco==2) {
+  for(i in 1:count)
+    cat("$")
+}else{
+  for(i in 1:count)
+    cat("#")
 }
 
 # 문제10 / hint: 추출된 값을 10으로 나눈 몫으로 비교
@@ -106,6 +133,19 @@ if(score%/%10 >= 9){
 }
 level <- switch(EXPR = score1,"10점"=,"9점"="A","8점"="B","7점"="C","6점"="D","F")
 cat(score," 점은 ",level,"등급입니다",sep="")
+
+# 문제10 - 정답2
+
+score <- sample(0:100,1)
+score 
+score <- score %/% 10
+level <- switch(EXPR=as.character(score), 
+                "10"=, "9"="A등급",
+                "8"="B등급",
+                "7"="C등급",
+                "6"="D등급",
+                "F등급")
+cat(score,"은 ",level,"입니다\n",sep="")
 
 # 문제11
 (a1 = LETTERS)
